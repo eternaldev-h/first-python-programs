8c8ce6f63a531418920d0b49d77e80d06e96d5be
eternaldev-h <agarwalhitansh17@gmail.com>
2026-05-09 21:46:49 +0530

    Added loop

--- calculator.py
+++ calculator.py
@@ -3,25 +3,34 @@ num2:int
 finalResult:int
 operator:str
 error:int
-num1=int(input("Enter number 1: "))
-operator=str(input("Enter operation: "))
-num2=int(input("Enter number 2: "))
-finalResult=0
-error=0
-if operator=="/":
-    finalResult=num1/num2
-elif operator=="*":
-    finalResult=num1*num2
-elif operator=="+":
-    finalResult=num1+num2
-elif operator=="-":
-    finalResult=num1-num2
-else:
-    error=error+1   #Code for error, debugged multiple times
-if error>0:
-    print("Error, write a logical operation")
-else:       
-    print(num1, operator, num2, "=", finalResult)
+loopCounter:int
+loopLogic:str
 
+error=0
+loopCounter=1
+while loopCounter==1:
+    num1=int(input("Enter number 1: "))
+    operator=str(input("Enter operation: "))
+    num2=int(input("Enter number 2: "))
+    finalResult=0
+    if operator=="/":
+        finalResult=num1/num2
+    elif operator=="*":
+        finalResult=num1*num2
+    elif operator=="+":
+        finalResult=num1+num2
+    elif operator=="-":
+        finalResult=num1-num2
+    else:
+        error=error+1   #Code for error, debugged multiple times
+    if error>0:
+        print("Error, write a logical operation")
+    else:       
+        print(num1, operator, num2, "=", finalResult)
+    loopLogic=str(input("Do you want to end the loop? Enter Y or N  "))
+    if loopLogic=="Y":  #Can add or for small Y(y) too
+        loopCounter=0
+    else:
+        loopCounter=1    
 
 #Can add a while loop to loop entire calculations. Can also add more operations like floor division and modulus
